a4ad24c1d53add8028ce634595b5c88680326acc
Karl Pokus <karl.pokus@gmail.com>
2016-07-03 21:44:39 +0200

    more tests

--- tests.js
+++ tests.js
@@ -1,7 +1,38 @@
 var test = require('tape'),
-    klocka = require('./klocka.js');
+    klocka = require('./klocka.js'),
+    ee = require('events'),
+    req1 = {url: '/items', method: 'GET'},
+    req2 = {url: '/items', method: 'POST'},
+    res1 = new ee(),
+    res2 = new ee(),
+    duration = 250,
+    stringToMS = function(str) {
+      // klocka GET/items: 2005.754ms
+      return +(str.split(':')[1].trim().replace('ms', ''));
+    };
+    require('console.mute');
 
-test('required', function(t){
+test('the basics', function(t){
   t.equal(typeof klocka, 'function', 'is function');
+
+  klocka(req1, res1, function(){
+    console.mute();
+    res1.emit('finish');
+    var data = console.resume();
+    var ms = stringToMS(data[0]);
+
+    t.ok(ms > 0, 'log is > 0');
+  });
+
+  klocka(req2, res2);
+  setTimeout(function(){
+    console.mute();
+    res2.emit('finish');
+    var data = console.resume();
+    var ms = stringToMS(data[0]);
+
+    t.ok(ms > duration, 'log is > duration');
+  }, duration);
+
   t.end();
 });
