ca029a951f7480af5f411927cc8d95303a3f07f0
Karl Pokus <karl.pokus@gmail.com>
2016-07-04 11:09:46 +0000

    add comment on express rewrites .url

--- klocka.js
+++ klocka.js
@@ -1,4 +1,6 @@
 module.exports = function(req, res, next) {
+  // if express is used .originalUrl is present and to be used, otherwise use .url
+  // Express rewrites .url See -> http://expressjs.com/en/4x/api.html#req.originalUrl
   var url = (req.originalUrl) ? req.originalUrl : req.url,
       name = "klocka " + req.method + url;
   console.time(name);
