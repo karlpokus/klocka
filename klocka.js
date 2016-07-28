9f35d9f40372674512db85121fa5d38d8ca95412
Karl Pokus <karl.pokus@gmail.com>
2016-07-28 14:28:41 +0200

    add unix ts to logged string

--- klocka.js
+++ klocka.js
@@ -2,7 +2,10 @@ module.exports = function(req, res, next) {
   // if express is used .originalUrl is present and to be used, otherwise use .url
   // Express rewrites .url See -> http://expressjs.com/en/4x/api.html#req.originalUrl
   var url = (req.originalUrl) ? req.originalUrl : req.url,
-      name = "klocka " + req.method + url;
+      d = new Date(),
+      t = d.getTime(),
+      name = "klocka (" + t + ") " + req.method + url;
+
   console.time(name);
   res.on('finish', function(){
     console.timeEnd(name);
